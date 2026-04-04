ff68dca35b78cda3a2cdd54c36eef00b495eb48d
VanBa311200 <70096921+VanBa311200@users.noreply.github.com>
2026-04-04 12:19:29 +0700

    fix(sentry): disable telemetry in Next.js config (#6)
    Disable Sentry telemetry in withSentryConfig to avoid sending plugin telemetry from this project.

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,8 @@ export default withSentryConfig(nextConfig, {
     deleteSourcemapsAfterUpload: true,
   },
 
+  telemetry: false,
+
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
